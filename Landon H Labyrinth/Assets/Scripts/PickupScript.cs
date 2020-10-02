d5db4a5efd1072e6d2e0c6cba1bfc34edc010e52
CodeNinjasSJ <57017893+CodeNinjasSJ@users.noreply.github.com>
2020-10-02 16:32:51 -0600

    Prove Yourself

---
+++ Landon H Labyrinth/Assets/Scripts/PickupScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupScript : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Coin;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.isTouching(Player)){
+            Destroy(Coin);
+        }
+    }
+}
